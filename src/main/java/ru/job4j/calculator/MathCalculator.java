0645ce7399ddf3fbdbc34c19f33fb1ea79d177ce
klimovape <klimpolinaevg@gmail.com>
2025-03-30 23:56:44 +0300

    1.1. Что такое import [#505278]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double minusAndDivide(double first, double second) {
+        return minus(first, second) + divide(first, second);
+    }
+
+    public static double sumOfAll(double first, double second) {
+        return sumAndMultiply(first, second) + minusAndDivide(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumOfAll(10, 20));
+    }
+}
