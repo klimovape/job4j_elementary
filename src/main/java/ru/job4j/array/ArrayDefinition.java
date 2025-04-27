9d96194f02e90b84803d9f1e30ef9e21329c552e
klimovape <klimpolinaevg@gmail.com>
2025-04-27 21:16:24 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,13 +3,16 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
 
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
+        String[] names = new String[4];
 
-        System.out.println("Размер массива ages равен: " + ages.length);
-        System.out.println("Размер массива surnames равен: " + surnames.length);
-        System.out.println("Размер массива prices равен: " + prices.length);
+        names[0] = "Anna";
+        names[1] = "Petr";
+        names[2] = "Polina";
+        names[3] = "Misha";
 
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
